9414c8a63367d23a9ab8ea7c82f887b22da1d604
cnorick <cnathanorick@gmail.com>
2017-06-17 15:17:36 -0700

    Configure publish script

--- publish.sh
+++ publish.sh
@@ -3,6 +3,5 @@
 
 flags="--delete"
 flags+=" --acl public-read"
-excludePattern="publish.sh"
 
-aws s3 sync . s3://nathanorick.com/ $flags --exclude $excludePattern
+aws s3 sync . s3://nathanorick.com/ $flags --exclude '.git/*' --exclude publish.sh
