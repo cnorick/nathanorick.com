224cc8e105402130f4048d3620ee6123638dccc8
Orick <corick@vols.utk.edu>
2017-08-16 19:56:57 -0400

    Add beginnings of Bio component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div>
       <intro class="intro"></intro>
+      <bio></bio>
       <copyright></copyright>
     </div>
   </div>
@@ -10,12 +11,14 @@
 <script>
 import Copyright from './components/Copyright/Copyright'
 import Intro from './components/Intro/Intro'
+import Bio from './components/Bio/Bio'
 
 export default {
   name: 'app',
   components: {
     Copyright,
-    Intro
+    Intro,
+    Bio
   }
 }
 </script>
---
+++ src/components/Bio/Bio.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bio">
+    <section>
+      <p>
+        I am a
+        <a href="http://www.eecs.utk.edu/">Computer Science</a> student at the
+        <a href="http://www.utk.edu/">University of Tennessee, Knoxville</a> with a minor in Math, graduating May 12th, 2018.
+        At UT, I am an ambassador for the <a>Engineering Professional Practice Office</a>.
+      </p>
+      <p>I worked four terms as an intern at <a>OSIsoft, LLC</a></p>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'bio',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+.intro {
+  padding-bottom: 40px;
+}
+</style>
