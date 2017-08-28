8716955c7339f7174fdfe0ac75243259e622267c
Nathan Orick <cnathanorick@gmail.com>
2017-08-28 17:42:54 -0400

    content content content

--- src/components/Bio/Bio.vue
+++ src/components/Bio/Bio.vue
@@ -5,14 +5,15 @@
         <p>
           I am a
           <a href="http://www.eecs.utk.edu/">Computer Science</a> student at the
-          <a href="http://www.utk.edu/">University of Tennessee, Knoxville</a> with a minor in Math, graduating May 12th, 2018. At UT, I am an ambassador for the
+          <a href="http://www.utk.edu/">University of Tennessee, Knoxville</a> minoring in Math, graduating May 12th, 2018. At UT, I am an ambassador for the
           <a href="http://www.coop.utk.edu/">Engineering Professional Practice Office</a> where I counsel engineering students in their internship searches.
         </p>
         <br/>
-        <p>I recently finished four terms as an intern at
-          <a href="http://www.osisoft.com/">OSIsoft, LLC</a>.
-          During my first three terms, I designed and implemented a 
+        <p>I recently finished four terms as a software development intern at
+          <a href="http://www.osisoft.com/">OSIsoft, LLC</a>. I worked three semesters in the Johnson City, TN office, and one semester at OSIsoft's HQ in San Leandro, CA. <!-- Check out what I worked on there below in the <a>projects</a> section -->
         </p>
+        <br/>
+        <p>I love programming, and I'm really excited work in software development professionally after graduation! I can't wait to put my skills and knowledge to good use on projects that will change the world.</p>
       </div>
     </div>
   </div>
