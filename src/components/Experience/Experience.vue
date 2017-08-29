f568caf3673bffc3e9d24cf43b1d8f1661485d6e
cnorick <cnathanorick@gmail.com>
2017-08-28 23:31:44 -0400

    Add dates to experience

--- src/components/Experience/Experience.vue
+++ src/components/Experience/Experience.vue
@@ -4,16 +4,19 @@
     <div class="columns is-marginless is-centered">
       <div class="column is-half-widescreen is-three-quarters-tablet">
         <div v-for="e in experiences" :key="e.id" class="box project" v-bind:id="e.id">
-          <div class="header">
-            <h3 class="title is-3">{{e.title}}</h3>
-            <h4 class="subtitle is-5">{{e.subtitle}}</h4>
+          <div class="header columns">
+            <div class="experience-title column is-one-third">
+              <h3 class="title is-3">{{e.title}}</h3>
+              <h4 class="subtitle is-5">{{e.subtitle}}</h4>
+            </div>
+            <div class="date column is-one-third is-offset-one-third"><span>{{e.startDate}}-{{e.endDate}}</span></div>
           </div>
           <hr>
           <div v-for="(p, i) in e.paragraphs">
             <p class="experience-paragraph">
               {{p}}
             </p>
-            <br v-if="i != e.paragraphs.length - 1"/>
+            <br v-if="i != e.paragraphs.length - 1" />
           </div>
         </div>
       </div>
@@ -34,6 +37,8 @@ export default {
           title: `OSIsoft`,
           subtitle: `Development Co-op`,
           id: 'OSI',
+          startDate: 'Jan 2016',
+          endDate: 'Aug 2017',
           paragraphs: [
             `I worked for twelve consecutive months at the Johnson City, TN office. There, I developed data-analysis
             software that collects, persists, analyzes, and visualizes customer and employee contribution and
@@ -60,6 +65,10 @@ export default {
   .project {
     .header {
       margin-bottom: 20px;
+      .experience-title {
+      }
+      .date {
+      }
     }
   }
 }
