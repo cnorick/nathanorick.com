3a2c6c103b183057c329c6871fcbc9dcb5c32399
cnorick <cnathanorick@gmail.com>
2017-08-30 18:33:05 -0400

    Add homepage tracking

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 import Copyright from './components/Copyright/Copyright'
 import Intro from './components/Intro/Intro'
 import Bio from './components/Bio/Bio'
+import Vue from 'vue'
 
 export default {
   name: 'app',
@@ -20,6 +21,10 @@ export default {
     Copyright,
     Intro,
     Bio
+  },
+  mounted: () => {
+    Vue.$ga.page('home')
+    Vue.$ga.event('event')
   }
 }
 </script>
