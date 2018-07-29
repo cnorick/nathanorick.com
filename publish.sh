04285ac9755a94d99080301939b0c3f8b4a37903
cnorick <cnathanorick@gmail.com>
2018-07-29 16:30:46 -0700

    Add publish script to package file

--- publish.sh
+++ publish.sh
@@ -5,4 +5,12 @@ flags="--delete"
 flags+=" --acl public-read"
 
 npm run build
-aws s3 sync dist/ s3://nathanorick.com/ $flags
+
+# Doesn't work right now because the app looks for assets at the root directory, not from /dev/.
+if [ $1 = "dev" ]; then
+    url="s3://content.nathanorick.com/dev/"
+else
+    url="s3://nathanorick.com/"
+fi
+
+aws s3 sync dist/ $url $flags
