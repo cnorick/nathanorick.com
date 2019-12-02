6f6ba7c3224967d54526b57a10613c78d2d936cd
Nathan Orick <norick@osisoft.com>
2019-12-02 09:13:43 -0800

    Update fiveinthehive

--- src/components/Experience/experiences.js
+++ src/components/Experience/experiences.js
@@ -4,9 +4,16 @@ export default [
     subtitle: `Fantasy Sports Web App`,
     id: '5inthehive',
     paragraphs: [
-      `Five in the Hive is ...`,
+      `My roommate, <a href="http://kanepenley.com/">Kane</a> loves football! For years, he and his friends have played a fantasy football game with a bit of a
+      twist. Each week during the season, each person chooses five NFL teams that they think will win against the Vegas spread for a specific game. In the past,
+      everyone shared their picks in a group message, and calculated a leaderboard at the end of each week and again at the end of the season,
+      but all that was a ton of work, so we made it easier!`,
 
-      `Click below to play!`
+      `Five in the Hive is Kane's game as a web app. The app is hosted completely on AWS. The front-end is in Angular, hosted in S3, and distributed with
+      CloudFront. The API is written in node, and hosted as a Lambda Function. The data-updating scripts are also hosted with Lambda but written in C#.
+      We use DynamoDB as the database.`,
+
+      `Click below to play! It's a PWA, so if you love it, you can install it to your phone.`
     ],
     links: [
       {
