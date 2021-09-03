84ac8df3146633249017c92e1309cc4618d7fda4
Nathan Orick <cnathanorick@gmail.com>
2021-09-02 23:26:31 -0400

    Consolidate theme changing

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,47 @@
   // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
+  let theme;
 
-  const toggleTheme = (state) => {
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+
+  const setTheme = (state) => {
+    theme = state;
     if (state === "dark") {
-      localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
-    } else if (state === "light") {
-      localStorage.setItem("theme", "dark");
       body.setAttribute("data-theme", "dark");
-    } else {
-      initTheme(state);
+    } else if (state === "light") {
+      body.removeAttribute("data-theme");
     }
   };
 
-  lamp.addEventListener("click", () =>
-    toggleTheme(localStorage.getItem("theme"))
-  );
+  const getInitialTheme = () => {
+    const localStorageTheme = localStorage.getItem("theme");
+    let systemTheme;
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      systemTheme = "dark";
+    }
+    return localStorageTheme || systemTheme || "light";
+  };
+
+  lamp.addEventListener("click", () => toggleTheme(theme));
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if(!localStorage.getItem("theme")) {
+      const newColorScheme = e.matches ? "dark" : "light";
+      setTheme(newColorScheme);
+    }
+  });
+
+  setTheme(getInitialTheme());
+
+  setTimeout(() => body.classList.remove("notransition"), 75);
 
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
